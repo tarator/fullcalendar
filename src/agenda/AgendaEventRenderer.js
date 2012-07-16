681c9f5c377658a5f5ffc88ad0de569342cbdea9
tarator <g.abenthung@tradescout.at>
2012-07-16 15:43:16 +0200

    IN_PROGRESS - bug 31: Fullcalendar für unsere Anforderungen anpassen. http://bugzilla.asap-soft.com/show_bug.cgi?id=31 Render Events on right kabine (Stacking and moving between cabins doesn't work yet...)

--- src/agenda/AgendaEventRenderer.js
+++ src/agenda/AgendaEventRenderer.js
@@ -285,7 +285,7 @@ function AgendaEventRenderer() {
 			return 0;
 		
 		for(var i = 0; i< kabinen.count; i++){
-			if(event.kabine == kabinen.name || event.kabine == i){
+			if(event.kabine == kabinen.names[i] || event.kabine == i){
 				return i;
 			}
 				
