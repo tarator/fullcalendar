e77b9c9d98cc73f871c1efbae3e8eb62e14203ea
tarator <g.abenthung@tradescout.at>
2012-10-27 15:53:36 +0200

    Send date in ms from 1970 to server

--- src/EventManager.js
+++ src/EventManager.js
@@ -128,10 +128,10 @@ function EventManager(options, _sources) {
 				var startParam = firstDefined(source.startParam, options.startParam);
 				var endParam = firstDefined(source.endParam, options.endParam);
 				if (startParam) {
-					data[startParam] = Math.round(+rangeStart / 1000);
+					data[startParam] = Math.round(+rangeStart);
 				}
 				if (endParam) {
-					data[endParam] = Math.round(+rangeEnd / 1000);
+					data[endParam] = Math.round(+rangeEnd);
 				}
 				pushLoading();
 				$.ajax($.extend({}, ajaxDefaults, source, {
